bf886f701194a4b26cfd3164dc4bfd0d406a3fe3
Vikas Pandey <vikaspa6@gmail.com>
2019-02-16 09:12:16 +0530

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -1,1 +1,4 @@
 Write-Host "This Msg comes from a remote script execution!!"
+
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/hclpandv/76739cc615f3eee0d4722243379d324f/raw/9573fa17aadee9f0b5fb71d38cccefde8b4a220b/CheckSumCalc.ps1'))
